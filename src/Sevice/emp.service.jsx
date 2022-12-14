678e7061380bc7436fe1fbe900aa382a26faa987
chethan <mchethan52@gmail.com>
2022-12-14 17:00:04 +0530

    init

---
+++ src/Sevice/emp.service.jsx
@@ -0,0 +1,27 @@
+import axios from "axios"
+
+const URL = "http://localhost:8080/api"
+
+class EmpService {
+  saveEmp(emp) {
+    return axios.post(`${URL}/save`, emp)
+  }
+
+  getAllEmp() {
+    return axios.get(`${URL}/all`)
+  }
+
+  getEmpById(id) {
+    return axios.get(`${URL}/${id}`)
+  }
+
+  deleteEmp(id) {
+    return axios.get(`${URL}/delete/${id}`)
+  }
+
+  updateEmp(id, emp) {
+    return axios.post(`${URL}/update/${id}`, emp)
+  }
+}
+
+export default new EmpService()
